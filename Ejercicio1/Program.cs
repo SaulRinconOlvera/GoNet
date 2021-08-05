368233874efc0c0a2adb683d298adea013996a8f
Saul Rincon <saulrincon@hotmail.com>
2021-08-05 04:44:30 -0500

    Ejercicio 1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fib(8));
+            Console.WriteLine(fib(7));
+            Console.WriteLine(fib(6));
+        }
+        private static int fib(int n) {
+            if(n<=2) return 1;
+            return fib(n-1) + fib(n-2);
+        }
+    }
+}
